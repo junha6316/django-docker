db0c50b967b16be2c341703c100f6dc45b203e51
root <root@ip-172-31-1-177.ap-northeast-2.compute.internal>
2021-07-29 10:46:53 +0000

    deploy settings

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
-yes | python manage.py makemigrations --settings=config.deploy.settings
+yes | python manage.py makemigrations --settings=test_page.settings
 echo "==> Django setup, executing: migrate pro"
-python manage.py migrate --settings=config.deploy.settings --fake-initial
+python manage.py migrate --settings=test_page.settings --fake-initial
 echo "==> Django setup, executing: collectstatic"
-python manage.py collectstatic --settings=config.deploy.settings --noinput -v 3
+python manage.py collectstatic --settings=test_page.settings --noinput -v 3
 pip install -r /code/requirements.txt
 echo "==> Django deploy"
-gunicorn -b 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=config.deploy.settings config.wsgi:application & daphne -b 0.0.0.0 -p 8009 config.asgi:application
+gunicorn -b 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=test_page.settings test_page.wsgi:application
